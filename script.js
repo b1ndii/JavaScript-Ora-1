9b3f2de0dce96c5ebf0aa69f6bffa5d0e3d70666
b1ndii <bind.berisha1@gmail.com>
2025-09-24 16:56:35 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+console.log("Andi" + 2);
+console.error("diqka nuk po punon")
+console.warn("kujdes semafori i verdhe")
+
+// var a = 2   ;
+// var b = 4;
+
+console.log(a + b)
+// alert("Rezultati juaj eshte " + (a + b));
+
+// alert(a+b);
+// alert(a<b)
+
+// var gjatesia;
+// console.log(gjatesia)
+
+// var height = 180;
+// var anotherHeight = height;
+// console.log(anotherHeight)
+
+// var emri = "Bind"
+// var telefoni = "044111111"
+// var email = "bindbb@gmail.com"
+// console.log("Emri:"+emri + ", Telefoni:" + telefoni + ", Email:" + email)
+
+var a = false;
+var b = true;
+var c = false;
+var d = true;
+
+// && - Edhe
+// || - Ose
+// ! - E kundert (Not)
+// == E kqyr a osht qashtu
+// === E kqyr a osht qashtu AMA e kqyr edhe tipin e tdhanes, a o numer a text a qka
+
+// console.log(a && b && c || d)
+// console.log(a && b && (c || d))
+// console.log(!(false && false && true))
+// console.log(10 === "10")
+// var emri = prompt("qysh e ki emrin:")
+// alert ("emri yt eshte: " + emri)
+
+
